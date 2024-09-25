ea4e07b71af8581a591de1b71c42dd673d372168
Super User <vedansh.kr312004@gmail.com>
2024-09-26 00:01:07 +0530

    Fixed Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import { Button } from './components/ui/button'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import { Button } from './components/ui/button'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import LandingPage from './landingPage.tsx'
 import '@fontsource/jost'; // Defaults to weight 400
 //import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
   return (
     <>
       <LandingPage></LandingPage>
